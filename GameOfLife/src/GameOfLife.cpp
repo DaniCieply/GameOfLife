c92e00d35a4b10cb33fec49a9867395fa2412288
Dani <danicieply@gmail.com>
2022-07-14 17:01:11 -0500

    added basic ui controls

--- GameOfLife/src/GameOfLife.cpp
+++ GameOfLife/src/GameOfLife.cpp
@@ -23,9 +23,19 @@ int genRandInt(int start, int end)
 int main()
 {
     sf::RenderWindow window(sf::VideoMode(GRID_WIDTH * 2, GRID_HEIGHT), "Conway's Game of Life");
-    sf::Clock clock;
 
-    window.setFramerateLimit(60);
+    sf::Clock frameClock; // Used for calculating Framerate
+    sf::Clock deltaClock; // ImGui clock
+    sf::Clock gameClock; // Game-speed calculations
+
+    ImGui::SFML::Init(window);
+
+    window.setVerticalSyncEnabled(true);
+
+    // Vars regarding whether sim should continue running or not
+    bool shouldRun = false;
+    bool enoughTimePass = false;
+    float gameSpeed = 1.0f;
 
     // Visual of game that is drawn to screen
     PointMap pointMap(GRID_WIDTH * GRID_HEIGHT);
@@ -58,97 +68,112 @@ int main()
     // Main Loop
     while (window.isOpen())
     {
-        // FPS calculation
-        float Framerate = 1.0f / clock.getElapsedTime().asSeconds();
-        clock.restart();
-
         // Event Handling
         sf::Event event;
         while (window.pollEvent(event))
         {
+            ImGui::SFML::ProcessEvent(event);
             if (event.type == sf::Event::Closed)
                 window.close();
         }
 
-        // Determines the which value the cell should change to
-        for (int x = 0; x < GRID_WIDTH; x++)
+        // FPS calculation
+        float Framerate = 1.0f / frameClock.getElapsedTime().asSeconds();
+        frameClock.restart();
+
+        // Limits speed of the simulation
+        if (gameClock.getElapsedTime().asMilliseconds() >= gameSpeed * 1000)
         {
-            for (int y = 0; y < GRID_HEIGHT; y++)
+            enoughTimePass = true;
+            gameClock.restart();
+        }
+        else
+        {
+            enoughTimePass = false;
+        }
+
+        if (shouldRun && enoughTimePass)
+        {
+            // Determines the which value the cell should change to
+            for (int x = 0; x < GRID_WIDTH; x++)
             {
-                int numAlive = 0;
-                int numDead = 0;
+                for (int y = 0; y < GRID_HEIGHT; y++)
+                {
+                    int numAlive = 0;
+                    int numDead = 0;
 
-                // Read the values of the front array and change the values of the back array
-                bool& frontCellVal = frontArray[x * GRID_WIDTH + y];
-                bool& backCellVal = backArray[x * GRID_WIDTH + y];
-                if (x - 1 >= 0 && y + 1 <= GRID_HEIGHT - 1) 
-                {
-                    if (frontArray[(x - 1) * GRID_WIDTH + (y + 1)]) numAlive++;
-                    else numDead++;
-                }
-                if (y + 1 <= GRID_HEIGHT - 1) 
-                {
-                    if (frontArray[x * GRID_WIDTH + (y + 1)]) numAlive++;
-                    else numDead++;
-                }
-                if (x + 1 <= GRID_WIDTH - 1 && y + 1 <= GRID_HEIGHT - 1)
-                {
-                    if (frontArray[(x + 1) * GRID_WIDTH + (y + 1)]) numAlive++;
-                    else numDead++;
-                }
-                if (x + 1 <= GRID_WIDTH - 1) 
-                {
-                    if (frontArray[(x + 1) * GRID_WIDTH + y]) numAlive++;
-                    else numDead++;
-                }
-                if (x + 1 <= GRID_WIDTH - 1 && y - 1 >= 0)
-                {
-                    if (frontArray[(x + 1) * GRID_WIDTH + (y - 1)]) numAlive++;
-                    else numDead++;
-                }
-                if (y - 1 >= 0)
-                {
-                    if (frontArray[x * GRID_WIDTH + (y - 1)]) numAlive++;
-                    else numDead++;
-                }
-                if (x - 1 >= 0 && y - 1 >= 0) 
-                {
-                    if (frontArray[(x - 1) * GRID_WIDTH + (y - 1)]) numAlive++;
-                    else numDead++;
-                }
-                if (x - 1 >= 0)
-                {
-                    if (frontArray[(x - 1) * GRID_WIDTH + y]) numAlive++;
-                    else numDead++;
-                }
+                    // Read the values of the front array and change the values of the back array
+                    bool& frontCellVal = frontArray[x * GRID_WIDTH + y];
+                    bool& backCellVal = backArray[x * GRID_WIDTH + y];
+                    if (x - 1 >= 0 && y + 1 <= GRID_HEIGHT - 1) 
+                    {
+                        if (frontArray[(x - 1) * GRID_WIDTH + (y + 1)]) numAlive++;
+                        else numDead++;
+                    }
+                    if (y + 1 <= GRID_HEIGHT - 1) 
+                    {
+                        if (frontArray[x * GRID_WIDTH + (y + 1)]) numAlive++;
+                        else numDead++;
+                    }
+                    if (x + 1 <= GRID_WIDTH - 1 && y + 1 <= GRID_HEIGHT - 1)
+                    {
+                        if (frontArray[(x + 1) * GRID_WIDTH + (y + 1)]) numAlive++;
+                        else numDead++;
+                    }
+                    if (x + 1 <= GRID_WIDTH - 1) 
+                    {
+                        if (frontArray[(x + 1) * GRID_WIDTH + y]) numAlive++;
+                        else numDead++;
+                    }
+                    if (x + 1 <= GRID_WIDTH - 1 && y - 1 >= 0)
+                    {
+                        if (frontArray[(x + 1) * GRID_WIDTH + (y - 1)]) numAlive++;
+                        else numDead++;
+                    }
+                    if (y - 1 >= 0)
+                    {
+                        if (frontArray[x * GRID_WIDTH + (y - 1)]) numAlive++;
+                        else numDead++;
+                    }
+                    if (x - 1 >= 0 && y - 1 >= 0) 
+                    {
+                        if (frontArray[(x - 1) * GRID_WIDTH + (y - 1)]) numAlive++;
+                        else numDead++;
+                    }
+                    if (x - 1 >= 0)
+                    {
+                        if (frontArray[(x - 1) * GRID_WIDTH + y]) numAlive++;
+                        else numDead++;
+                    }
 
-                // Determines whether the cell being analyzed should be dead or alive
-                if (frontCellVal)
-                {
-                    if (numAlive < 2)
+                    // Determines whether the cell being analyzed should be dead or alive
+                    if (frontCellVal)
                     {
-                        backCellVal = false;
+                        if (numAlive < 2)
+                        {
+                            backCellVal = false;
+                        }
+                        else if (numAlive == 2 || numAlive == 3)
+                        {
+                            backCellVal = true;
+                        }
+                        else if (numAlive > 3)
+                        {
+                            backCellVal = false;
+                        }
                     }
-                    else if (numAlive == 2 || numAlive == 3)
+                    else
                     {
-                        backCellVal = true;
-                    }
-                    else if (numAlive > 3)
-                    {
-                        backCellVal = false;
-                    }
-                }
-                else
-                {
-                    if (numAlive == 3)
-                    {
-                        backCellVal = true;
+                        if (numAlive == 3)
+                        {
+                            backCellVal = true;
+                        }
                     }
                 }
             }
-        }
 
-        frontArray = backArray;
+            frontArray = backArray;
+        }
         
         // Changes colors of point map based upon front array
         for (int i = 0; i < GRID_WIDTH * GRID_HEIGHT; i++) 
@@ -163,13 +188,38 @@ int main()
             }
         }
 
+        ImGui::SFML::Update(window, deltaClock.restart());
+
+        // Sim Options UI
+        ImGui::Begin("Options");
+        ImGui::SetWindowPos(ImVec2(GRID_WIDTH + 10, 10));
+        ImGui::SetWindowSize(ImVec2(GRID_WIDTH / 1.1f, GRID_HEIGHT / 1.1f));
+
+        ImGui::SliderFloat("Sim Speed", &gameSpeed, 0.0f, 10.0f);
+        if (ImGui::IsItemHovered(ImGuiHoveredFlags_AllowWhenDisabled))
+        {
+            ImGui::SetTooltip("How many seconds between game updates");
+        }
+
+        ImGui::Checkbox("Running", &shouldRun);
+        // Exit Button
+        bool winShouldClose = ImGui::Button("Exit");
+        ImGui::End();
+
+        if (winShouldClose)
+        {
+            window.close();
+        }
+
+        std::cout << Framerate << std::endl;
+
         // Draw
         window.clear();
         window.draw(pointMap);
+        ImGui::SFML::Render(window);
         window.display();
-
-        std::cout << Framerate << std::endl;
     }
 
+    ImGui::SFML::Shutdown();
     return 0;
 }
