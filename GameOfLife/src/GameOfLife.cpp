95cbc738df197150302ba33b1969ac72865987f7
Dani <danicieply@gmail.com>
2022-07-15 00:33:17 -0500

    added color change, save/load, and various other config options

--- GameOfLife/src/GameOfLife.cpp
+++ GameOfLife/src/GameOfLife.cpp
@@ -1,14 +1,15 @@
 #include <SFML/Graphics.hpp>
 #include <imgui.h>
 #include <imgui-SFML.h>
+#include <ImGuiFileDialog.h>
 #include <random>
 #include <array>
-#include <iostream>
-#include <memory>
+#include <fstream>
 #include "PointMap.h"
 
 using std::array;
 using std::vector;
+using std::fstream;
 
 const int GRID_WIDTH = 500;
 const int GRID_HEIGHT = 500;
@@ -18,6 +19,66 @@ int genRandInt(int start, int end)
     std::random_device rd;
     std::default_random_engine ran(rd());
     return std::uniform_int_distribution<>{start, end}(ran);
+}
+
+void saveGrid(array<bool, GRID_WIDTH* GRID_HEIGHT>& sArray, std::string filePath)
+{
+    fstream file;
+    file.open(filePath, std::ios::out);
+    if (file.is_open())
+    {
+        for (bool b : sArray)
+        {
+            file << b;
+        }
+    }
+
+    file.close();
+}
+
+void loadGrid(array<bool, GRID_WIDTH* GRID_HEIGHT>& lArray, std::string filePath)
+{
+    fstream file;
+    file.open(filePath, std::ios::in);
+    if (file.is_open())
+    {
+        std::string line;
+        std::getline(file, line);
+
+        int i = 0;
+        for (char c : line)
+        {
+            if (c == '0')
+            {
+                lArray[i] = false;
+            }
+            else if (c == '1')
+            {
+                lArray[i] = true;
+            }
+
+            i++;
+        }
+    }
+
+    file.close();
+}
+
+
+void randomizeArray(array<bool, GRID_WIDTH * GRID_HEIGHT>& array, int aliveChance)
+{
+    for (int i = 0; i < GRID_WIDTH * GRID_HEIGHT; i++)
+    {
+        int randNum = genRandInt(1, 10);
+        if (randNum <= aliveChance)
+        {
+            array[i] = true;
+        }
+        else
+        {
+            array[i] = false;
+        }
+    }
 }
 
 int main()
@@ -37,6 +98,11 @@ int main()
     bool enoughTimePass = false;
     float gameSpeed = 1.0f;
 
+    // Generation vars
+    int aliveChance = 5;
+    float liveCellColor[3] = { (float)255 / 255, (float)255 / 255, (float)255 / 255 };
+    float deadCellColor[3] = { (float)0 / 255, (float)0 / 255, (float)0 / 255 };
+
     // Visual of game that is drawn to screen
     PointMap pointMap(GRID_WIDTH * GRID_HEIGHT);
     pointMap.setAllPointColor(sf::Color::Transparent);
@@ -45,19 +111,7 @@ int main()
     array<bool, GRID_WIDTH * GRID_HEIGHT> frontArray;
     array<bool, GRID_WIDTH * GRID_HEIGHT> backArray;
 
-    // Gives each cell a random starting value in the front array
-    for (int i = 0; i < GRID_WIDTH * GRID_HEIGHT; i++) 
-    {
-        int randNum = genRandInt(1, 13);
-        if (randNum <= 3)
-        {
-            frontArray[i] = true;
-        }
-        else 
-        {
-            frontArray[i] = false;
-        }
-    }
+    randomizeArray(frontArray, aliveChance);
 
     // Sets all back array cells false by default
     for (int i = 0; i < backArray.size(); i++)
@@ -191,28 +245,83 @@ int main()
         ImGui::SFML::Update(window, deltaClock.restart());
 
         // Sim Options UI
-        ImGui::Begin("Options");
+        // Main Options Window
+        ImGui::Begin("Main Options");
         ImGui::SetWindowPos(ImVec2(GRID_WIDTH + 10, 10));
-        ImGui::SetWindowSize(ImVec2(GRID_WIDTH / 1.1f, GRID_HEIGHT / 1.1f));
+        ImGui::SetWindowSize(ImVec2(GRID_WIDTH / 2.25f, GRID_HEIGHT / 2.5f));
         std::string fpsString = "FPS: " + std::to_string(Framerate);
         ImGui::Text(fpsString.c_str());
 
-        ImGui::SliderFloat("Sim Speed", &gameSpeed, 0.0f, 10.0f);
+        ImGui::SliderFloat("Speed", &gameSpeed, 0.0f, 10.0f);
         if (ImGui::IsItemHovered(ImGuiHoveredFlags_AllowWhenDisabled))
         {
             ImGui::SetTooltip("How many seconds between game updates");
         }
 
         ImGui::Checkbox("Running", &shouldRun);
-        // Exit Button
-        bool winShouldClose = ImGui::Button("Exit");
-        ImGui::End();
 
-        if (winShouldClose)
+        // Save current grid
+        if (ImGui::Button("Save Grid"))
+            ImGuiFileDialog::Instance()->OpenDialog("ChooseFileDlgKey", "Select file", ".clg", ".");
+        if (ImGuiFileDialog::Instance()->Display("ChooseFileDlgKey"))
+        {
+            std::string filePath;
+            if (ImGuiFileDialog::Instance()->IsOk())
+            {
+                filePath = ImGuiFileDialog::Instance()->GetFilePathName();
+            }
+
+            saveGrid(frontArray, filePath);
+            ImGuiFileDialog::Instance()->Close();
+        }
+        
+        // Load saved grid
+        if (ImGui::Button("Load Grid"))
+            ImGuiFileDialog::Instance()->OpenDialog("ChooseFileDlgKey2", "Select file", ".clg", ".");
+        if (ImGuiFileDialog::Instance()->Display("ChooseFileDlgKey2"))
+        {
+            std::string filePath;
+            if (ImGuiFileDialog::Instance()->IsOk())
+            {
+                filePath = ImGuiFileDialog::Instance()->GetFilePathName();
+            }
+
+            loadGrid(frontArray, filePath);
+            ImGuiFileDialog::Instance()->Close();
+        }
+
+        // Exit Button
+        if (ImGui::Button("Exit"))
         {
             window.close();
         }
+        ImGui::End();
 
+        // Generation Options Window
+        ImGui::Begin("Generation Options");
+        ImGui::SetWindowPos(ImVec2(GRID_WIDTH + 10, 215));
+        ImGui::SetWindowSize(ImVec2(GRID_WIDTH / 1.1f, GRID_HEIGHT / 2.5f));
+        ImGui::SliderInt("Alive Chance", &aliveChance, 1, 10);
+
+        // Color Options
+        ImGui::ColorEdit3("Live Cell Color", liveCellColor);
+        ImGui::ColorEdit3("Dead Cell Color", deadCellColor);
+        for (int i = 0; i < GRID_WIDTH * GRID_HEIGHT; i++)
+        {
+            if (frontArray[i])
+                pointMap.setPointColor(i, sf::Color((int)(liveCellColor[0] * 255), (int)(liveCellColor[1] * 255), (int)(liveCellColor[2] * 255)));
+            else
+                pointMap.setPointColor(i, sf::Color((int)(deadCellColor[0] * 255), (int)(deadCellColor[1] * 255), (int)(deadCellColor[2] * 255)));
+        }
+
+        // Randomizes the sim on click
+        if (ImGui::Button("Randomize"))
+        {
+            randomizeArray(frontArray, aliveChance);
+        }
+        
+        ImGui::End();
+        
         // Draw
         window.clear();
         window.draw(pointMap);
